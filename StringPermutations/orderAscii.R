c34ac43f1b3cc4b2c1d10211c765c62fb7a61986
Toti Cavalcanti <toticavalcanti@hotmail.com>
2017-05-16 14:05:00 -0300

    StringPermutations

---
+++ StringPermutations/orderAscii.R
@@ -0,0 +1,9 @@
+asciiSort <- function(lista) {
+  for(i in 1:length(lista)){
+    listOrder <- paste(listOrder, utf8ToInt(lista[i]))
+  }
+  for(i in 1:length(lista)){
+    listOrder <- paste(listOrder, utf8ToInt(lista[i]))
+  }
+}
+
